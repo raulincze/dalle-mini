ba12dc0f29b8ba3d89fe4b05670bb2a460bfd5ca
raulincze <raulincze@gmail.com>
2022-07-25 18:23:49 +0300

    Added new tokenizer to init.

--- src/dalle_mini/model/__init__.py
+++ src/dalle_mini/model/__init__.py
@@ -2,4 +2,4 @@ from .configuration import DalleBartConfig
 from .modeling import DalleBart
 from .partitions import set_partitions
 from .processor import DalleBartProcessor
-from .tokenizer import DalleBartTokenizer
+from .tokenizer import DalleBartTokenizer, GameCoordinateTokenizer
